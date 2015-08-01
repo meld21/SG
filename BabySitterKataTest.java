65df80ad34a22c5b92b4273aed1617328462b41d
meld21 <melanie.devries85@gmail.com>
2015-07-31 21:35:30 -0500

    Update BabySitterKataTest.java
    added new failing test babySitterDoesNotStartWorkUntil5PM()

--- BabySitterKataTest.java
+++ BabySitterKataTest.java
@@ -3,7 +3,12 @@ package babysitterkata;
 
 
 public class BabySitterKataTest {
-    
+
+    public void babySitterDoesNotStartWorkUntil5PM()
+    {
+        EveningOfBabySitting evening = new EveningOfBabySitting();
+        assertIsEqual(evening.setStartTime(5, 0), false);
+    }
     
     private <E extends Object> void assertIsEqual(E value1, E value2)
     {
